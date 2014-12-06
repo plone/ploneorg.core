ca81f2b3093a7fbeab6720b54c7639b5af34bb09
Gil Forcada <gforcada@gnome.org>
2014-12-06 02:34:27 +0100

    Add missing utf-8 headers

--- ploneorg/core/browser/homepage.py
+++ ploneorg/core/browser/homepage.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
--- ploneorg/core/browser/viewlets.py
+++ ploneorg/core/browser/viewlets.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from plone.app.layout.viewlets import ViewletBase
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
