0cce7bb5f4d8a75f1da198138b838e85f653a396
Gil Forcada <gforcada@gnome.org>
2015-04-27 20:26:08 +0200

    pep8 lines too long

--- ploneorg/core/browser/homepage.py
+++ ploneorg/core/browser/homepage.py
@@ -14,17 +14,21 @@ class HomePage(BrowserView):
 
     def get_events(self):
         pc = api.portal.get_tool('portal_catalog')
-        result = pc.searchResults(portal_type='Event',
-                                  end={'query': datetime.now(), 'range': 'min'},
-                                  sort_on='start',
-                                  review_state='published')
+        result = pc.searchResults(
+            portal_type='Event',
+            end={'query': datetime.now(), 'range': 'min'},
+            sort_on='start',
+            review_state='published'
+        )
 
         return result[:4]
 
     def get_news(self):
         pc = api.portal.get_tool('portal_catalog')
-        result = pc.searchResults(portal_type='News Item',
-                                  sort_on='Date',
-                                  sort_order='reverse',
-                                  review_state='published')
+        result = pc.searchResults(
+            portal_type='News Item',
+            sort_on='Date',
+            sort_order='reverse',
+            review_state='published'
+        )
         return result[:4]
