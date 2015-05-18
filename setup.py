ea0ee62d15d95a8eb2a8c39b2b10cd35f43da409
Jens W. Klein <jk@kleinundpartner.at>
2015-05-18 16:38:04 +0200

    houskeeping: sorting dependencies to the places they belong to.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from setuptools import setup, find_packages
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
 import os
 
 version = '1.0a1'
@@ -6,47 +9,53 @@ version = '1.0a1'
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
 
-setup(name='ploneorg.core',
-      version=version,
-      description="plone.org 2014 site core",
-      long_description=README + "\n" + HISTORY,
-      classifiers=[
-          "Framework :: Plone",
-          "Programming Language :: Python",
-      ],
-      keywords='plone ploneorg diazo theme',
-      author='Plone Foundation',
-      author_email='foundation@plone.org',
-      url='https://github.com/plone/ploneorg.core',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['ploneorg'],
-      include_package_data=True,
-      zip_safe=False,
-      extras_require={'test': ['plone.app.testing[robot]>=4.2.2']},
-      install_requires=[
-          'setuptools',
-          'ploneorg.theme',
-          'plone.api',
-          'plone.app.contenttypes',
-          'plone.app.event[dexterity]',
-          'plone.app.referenceablebehavior',
-          'plone.app.widgets[dexterity]',
-          'plone.directives.form',
-          'wildcard.foldercontents',
-          'collective.monkeypatcher',
-          'PyGithub',
-          'requests',
-          'pycountry',
-          'plone.formwidget.multifile',
-          'Py-StackExchange',
-          'plone.app.vulnerabilities',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      [console_scripts]
-      update_contributions = ploneorg.core.contributions:update_contributions
-      """,
-      )
+setup(
+    name='ploneorg.core',
+    version=version,
+    description="plone.org 2014 site core",
+    long_description=README + "\n" + HISTORY,
+    classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+    ],
+    keywords='plone ploneorg diazo theme',
+    author='Plone Foundation',
+    author_email='foundation@plone.org',
+    url='https://github.com/plone/ploneorg.core',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['ploneorg'],
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'test': ['plone.app.testing[robot]>=4.2.2'],
+        'migration': ['ploneorg.migration'],
+    },
+    install_requires=[
+        'Py-StackExchange',
+        'PyGithub',
+        'collective.monkeypatcher',
+        'collective.roster',
+        'collective.workspace',
+        'plone.api',
+        'plone.app.contenttypes',
+        'plone.app.event[dexterity]',
+        'plone.app.referenceablebehavior',
+        'plone.app.vulnerabilities',
+        'plone.app.widgets[dexterity]',
+        'plone.directives.form',
+        'plone.formwidget.multifile',
+        'ploneorg.theme',
+        'pycountry',
+        'requests',
+        'setuptools',
+        'wildcard.foldercontents',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    [console_scripts]
+    update_contributions = ploneorg.core.contributions:update_contributions
+    """,
+)
