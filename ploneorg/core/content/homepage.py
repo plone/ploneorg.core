3bdf91255d973d628a3b8920bfe4eb69f9935930
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-11-09 14:34:15 +0100

    Dynamic homepage
    - Add content type fields.
    - Make template comatible with theme.
    - Add Diazo rules
    - Add setup handlers

--- ploneorg/core/content/homepage.py
+++ ploneorg/core/content/homepage.py
@@ -1,16 +1,128 @@
 # -*- coding: utf-8 -*-
-from plone.app.contenttypes.interfaces import IDocument
+from zope.interface import implements
+from plone.app.textfield import RichText
 from plone.dexterity.content import Item
-from zope.interface import implementer
+from zope import schema
+from plone.supermodel.directives import fieldset
+from plone.supermodel.model import Schema
+from z3c.relationfield.schema import RelationChoice
 
 
-class IHomePage(IDocument):
+class IHomePage(Schema):
     """Marker for homepage schema"""
 
+    # Download
 
-@implementer(IHomePage)
+    fieldset(
+        'Download',
+        label=u'Download',
+        fields=[
+            'download_title',
+            'download_text',
+            'download_button_text',
+            'download_url',
+            'download_below_button_text',
+        ]
+    )
+    download_title = schema.TextLine(
+        title=u"Title",
+        required=True,
+    )
+    download_text = RichText(
+        title=u"Text",
+        required=True,
+    )
+    download_button_text = schema.TextLine(
+        title=u"Button text",
+        required=True,
+    )
+    download_url = schema.URI(
+        title=u"Download URL",
+        required=True,
+    )
+    download_below_button_text = RichText(
+        title=u"Text below download button",
+        required=True,
+    )
+
+    # Events
+
+    fieldset(
+        'Events',
+        label=u'Events',
+        fields=[
+            'event_title',
+            'event_text',
+            'event_page',
+        ]
+    )
+    event_title = schema.TextLine(
+        title=u"Title",
+        required=True,
+    )
+    event_text = RichText(
+        title=u"Text",
+        required=True,
+    )
+    event_page = RelationChoice(
+        title=u"Events page",
+        vocabulary="plone.app.vocabularies.Catalog",
+        required=True,
+    )
+
+    # Community
+
+    fieldset(
+        'Community',
+        label=u'Community',
+        fields=[
+            'community_title',
+            'community_text',
+            'community_page',
+        ]
+    )
+    community_title = schema.TextLine(
+        title=u"Title",
+        required=True,
+    )
+    community_text = RichText(
+        title=u"Text",
+        required=True,
+    )
+    community_page = RelationChoice(
+        title=u"Community page",
+        vocabulary="plone.app.vocabularies.Catalog",
+        required=False,
+    )
+
+    # Sponsors
+
+    fieldset(
+        'Sponsors',
+        label=u'Sponsors',
+        fields=[
+            'sponsor_title',
+            'sponsor_text',
+            'sponsor_page',
+        ]
+    )
+    sponsor_title = schema.TextLine(
+        title=u"Title",
+        required=True,
+    )
+    sponsor_text = RichText(
+        title=u"Text",
+        required=True,
+    )
+    sponsor_page = RelationChoice(
+        title=u"Sponsor page",
+        vocabulary="plone.app.vocabularies.Catalog",
+        required=True,
+    )
+
+
 class HomePage(Item):
-    """Specific marked HomePage Base class
-
-    (why not use a behavior to mark Item?)
     """
+    Dexterity content item for Cases
+    """
+    implements(IHomePage)
