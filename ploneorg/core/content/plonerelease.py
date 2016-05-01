9e299e553d946c1c60819199b622a602fa6feca5
esteele <eric@esteele.net>
2016-05-01 11:21:38 -0400

    Add download link grid field

--- ploneorg/core/content/plonerelease.py
+++ ploneorg/core/content/plonerelease.py
@@ -6,6 +6,33 @@ from plone.supermodel.model import Schema
 from ploneorg.core import _
 from zope import schema
 from zope.interface import implementer
+from collective.z3cform.datagridfield import DictRow
+from collective.z3cform.datagridfield.datagridfield import \
+        DataGridFieldFactory
+from plone.directives import form
+from ploneorg.core.vocabularies import platform_vocabulary
+
+
+class IReleaseUpload(Schema):
+    """ File download link for a Plone release
+    """
+    description = schema.TextLine(
+        title=_(u'Description'),
+        required=False
+    )
+    platform = schema.Choice(
+        title=_(u'Platform'),
+        vocabulary = platform_vocabulary,
+        required=False
+    )
+    url = schema.TextLine(
+        title=_(u'URL'),
+        required=False
+    )
+    file_size = schema.TextLine(
+        title=_(u'File size'),
+        required=False
+    )
 
 
 class IPloneRelease(Schema):
@@ -38,6 +65,12 @@ class IPloneRelease(Schema):
         required=False,
     )
 
+    form.widget(files=DataGridFieldFactory)
+    files = schema.List(
+        title=_(u'Files'),
+        value_type=DictRow(title=_(u'Uploads'), schema=IReleaseUpload),
+        required=False
+    )
 
 @implementer(IPloneRelease)
 class PloneRelease(Item):
